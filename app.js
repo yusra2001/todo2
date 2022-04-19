6aaae05606b9d51855d2edaca7b89b9b1d802b36
yusra2001 <yusrahadidi@gmail.com>
2022-04-19 16:40:56 +0300

    push

---
+++ app.js
@@ -0,0 +1,59 @@
+console.log("yusra");
+////////////////////
+var username = prompt("what's yout name?");
+console.log(username);
+//////////////////////
+
+var usergender = prompt("What is your gender? Male/Female")
+if (usergender == "Male" || usergender == "male") {
+ console.log(usergender);
+    alert(`welcome to my website Mr.${username}`)
+
+}
+else if (usergender == "Female" || usergender == "female") {
+
+    alert(`welcome to my website Ms.${username}`)
+}
+else {
+    alert(`welcome to my website `)
+
+}
+
+//////////////
+console.log("test");
+////////////////
+var userage = prompt("enter your age")
+if (userage <= 0) {
+    alert("You are under age!")
+
+}
+////////////////////////
+var password = prompt("enter your password 1234")
+if (password == 1234) {
+    alert("welcome to my website")
+}
+else {
+    alert("wrong password,try again")
+}
+////////////////
+//////////////
+
+var usergender = prompt("DO wants to skip the welcoming message?")
+////////////////
+
+
+var lastmsg = prompt("welcome to the website")
+if (usergender == "Male" || usergender == "male") {
+    alert("welcome to my website Mr.", username)
+
+}
+else if (usergender == "Female" || usergender == "female") {
+
+    alert("welcome to my website Ms.", username)
+}
+else {
+    alert("welcome to my website ", username)
+
+}
+
+/////////////////////////
